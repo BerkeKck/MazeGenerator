e195b62e855415e31d1a9c0f0f703a653c6fd48a
Berke Küçük <84639060+BerkeKck@users.noreply.github.com>
2024-04-22 16:31:20 +0300

    Object Oriented

---
+++ OOP-master/OOP_Giris/BilgisayarParcalari/AnaKart.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Giris.BilgisayarParcalari
+{
+    public enum RamTipi:byte
+    {
+        DDR1=1,
+        DDR2=2, 
+        DDR3=3, 
+        DDR4=4, 
+        DDR5=5, 
+        DDR6=6
+    }
+
+    public class AnaKart
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public RamTipi RamTipi { get; set; }
+        public bool Wifi { get; set; }
+      
+
+        public float Fiyat { get; set; }
+    }
+}
