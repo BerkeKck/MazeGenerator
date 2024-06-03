addb2f05e1a310d0be95c65cc090ec422c0ead2a
Berke Küçük <84639060+BerkeKck@users.noreply.github.com>
2024-06-03 14:37:17 +0300

    WebMvc

---
+++ CompanyInfo.Entities/DbContexts/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using CompanyInfo.Entities.EntityConfig.Concrete;
+using CompanyInfo.Entities.Models.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyInfo.Entities.DbContexts
+{
+    public class AppDbContext:DbContext
+    {
+
+        public DbSet<Urun>  Urunler{ get; set; }
+        public DbSet<Kategori> Kategoriler { get; set; }
+        public DbSet<Tedarikci> Tedarikciler { get; set; }
+        public DbSet<Birim> Birimler { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=IstkaCompanyInfo;Trusted_Connection=true;
+                TrustServerCertificate=true");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new BirimConfig());
+            //modelBuilder.ApplyConfiguration(new KategoriConfig());
+           
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+        }
+    }
+}
