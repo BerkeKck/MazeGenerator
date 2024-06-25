8164ad1e55eccaccd1b34d04c5097d3049996be6
BerkeKck <brke.kucuk.22@gmail.com>
2024-06-25 15:49:27 +0300

    NorthwindApi
    new project

---
+++ NorthWind.Api/NorthWindApiConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NorthWindApiConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
