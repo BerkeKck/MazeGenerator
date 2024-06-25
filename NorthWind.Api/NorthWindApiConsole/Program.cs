cae2fc0704a73759c2782de00304fe7cc75328b8
BerkeKck <brke.kucuk.22@gmail.com>
2024-06-25 17:20:54 +0300

    Api try

--- NorthWind.Api/NorthWindApiConsole/Program.cs
+++ NorthWind.Api/NorthWindApiConsole/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace NorthWindApiConsole
+﻿using NorthWindApi.Console;
+using System.Text.Json;
+
+namespace NorthWindApiConsole
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args
         {
-            Console.WriteLine("Hello, World!");
+            GetCategories();
+        }
+              static void GetCategories()
+            {
+                HttpClient httpClient = new HttpClient(); // HttpClient 
+                httpClient.BaseAddress = new Uri("https://northwind.now.sh/");
+
+                var result = httpClient.GetAsync("api/categories").Result;// Async metodunun direk sonucunu almaya zorlar 
+
+                var jsonString = result.Content.ReadAsStringAsync().Result;
+
+                var categories = JsonSerializer.Deserialize<List<Category>>(jsonString);
+
+                foreach (var category in categories)
+                    System.Console.WriteLine(
+                        $"Id: {category.id} - " +
+                        $"Name: {category.name} - " +
+                        $"Description:{category.description}");
+
+            }
         }
     }
 }
+    
