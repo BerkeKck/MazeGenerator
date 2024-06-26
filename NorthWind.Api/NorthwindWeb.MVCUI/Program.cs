0ae37f7415b89d0ee161fe742c1e7396d824f349
BerkeKck <brke.kucuk.22@gmail.com>
2024-06-26 17:32:29 +0300

    scaffold+DAL

--- NorthWind.Api/NorthwindWeb.MVCUI/Program.cs
+++ NorthWind.Api/NorthwindWeb.MVCUI/Program.cs
@@ -8,7 +8,7 @@ namespace NorthwindWeb.MVCUI
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
-
+              
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
