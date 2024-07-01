44bd536815c486f602780c6682a81d5638fca662
BerkeKck <brke.kucuk.22@gmail.com>
2024-07-01 15:32:39 +0300

    JWTBearer

---
+++ NorthWind.Api/NorthWind.Api/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.BL.Abstract;
+using Northwind.Entites.Sakila;
+
+namespace NorthWind.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController(IManager<SakilaContext,Customer> manager) : ControllerBase
+    {
+        [HttpGet]
+       public async Task<IActionResult> Index()
+        {
+            var result = await  manager.GetAllAsync(null);
+            return Ok(result);
+        }
+    }
+}
--- NorthWind.Api/NorthWind.Api/Program.cs
+++ NorthWind.Api/NorthWind.Api/Program.cs
@@ -1,14 +1,17 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Northwind.BL.Abstract;
 using Northwind.BL.Concrete;
 using Northwind.Entites.Entities;
 using Northwind.Entites.Sakila;
+using System.Text;
 
 namespace NorthWind.Api
 {
     public class Program
-    { 
+    {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
@@ -34,6 +37,20 @@ namespace NorthWind.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddJwtBearer(options => options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+               {
+                   ValidateAudience = true,// token üzerinde Audience do?rulamas?n? aktifle?tirdik.
+                   ValidateIssuer = true,//token üzerinde Issuer do?rulamas?n? aktifle?tirdik.
+                   ValidateLifetime = true,// token de?erinin kullan?m süresi do?rulamas?n? aktifle?tirdik.
+                   ValidateIssuerSigningKey = true,//token de?erinin bu uygulamaya ait olup olmad???n? anlamam?z? sa?layan Security Key do?rulamas?n? aktifle?tirdik.
+                   ValidIssuer = "https://localhost:7022",//uygulamadaki token?n Issuer de?erini belirledik.
+                   ValidAudience = "https://localhost:7022",//uygulamadaki token?n Audience de?erini belirledik.
+                   IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Benim Super Sifrem Benim Super Sifrem Benim Super Sifrem")),// Security Key do?rulamas? için SymmetricSecurityKey nesnesi arac?l???yla mevcut keyi belirtiyoruz.
+                   ClockSkew = TimeSpan.Zero //TimeSpan.Zero de?eri ile token süresinin üzerine ekstra bir zaman eklemeksizin s?f?r de?erini belirtiyoruz.
+
+               });
+
 
             var app = builder.Build();
 
